6ba5013f25cab3a2dc31a24cba14bac403768e41
dburnaye <dburnaye@gmu.edu>
2025-01-01 17:59:30 -0500

    Challenge #7 Step 1 changes (1)

--- index.js
+++ index.js
@@ -189,8 +189,11 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
     return item2.pledged - item1.pledged;
 });
 
-// use destructuring and the spread operator to grab the first and second games
+// use destructuring and the spread operator to grab the first and second MOST FUNDED games
+var first, second, rest;
+[first, second, ...rest] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+
 
 // do the same for the runner up item

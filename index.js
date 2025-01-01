909601544fa7e369c6b8cc92a4d923f33f219efc
dburnaye <dburnaye@gmu.edu>
2025-01-01 02:44:27 -0500

    Challenge #5 Step 3 changes (1)

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
-
+    addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
